f9ea05bf5133b91feffacb4ba20a834fb89ba591
chenkongliang <victoryckl@tpxip.com>
2015-03-21 11:06:38 +0800

    modify font size

--- HelloServlet2/WEB-INF/classes/HelloServlet.java
+++ HelloServlet2/WEB-INF/classes/HelloServlet.java
@@ -8,7 +8,7 @@ public class HelloServlet extends HttpServlet {
 		throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
-		out.println("<font size=\"20\" color=blue>web-demos\\HelloServlet2\\WEB-INF\\classes\\HelloServlet.java</font><br>");
+		out.println("<p style=\"font-size:20px\">web-demos\\HelloServlet2\\WEB-INF\\classes\\HelloServlet.java</p>");
 		out.println("<marquee>"+new java.util.Date()+"</marquee>");
 		out.println("</html>");
 	}
